fc7dd224ade9e9797d59ff6cc2cf8b9afbbcb6b5
allievicky <78673964+allievicky@users.noreply.github.com>
2025-05-25 06:29:02 -0500

    Inject the forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -44,7 +44,29 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"]
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml = 
+    forecastHtml +
+    `
+    <div class="weather-forecast-day">
+        <div class="weather-forecast-date">Tues</div>
+        <div class="weather-forecast-icon">⛅️</div>
+        <div class="weather-forecast-temperatures"><strong>15°</strong></div>
+        <div class="weather-forecast-temperatures">9°</div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Lisbon");
+displayForecast();
